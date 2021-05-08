ab3e0ad255438b7295ba95e9742d31aeeb31130c
Matt Salisbury <matty8salisbury@gmail.com>
2021-05-08 15:02:06 +0100

    Deleting venueinfo file.  Users will create instead of downloading.

--- venueinfo.R
+++ venueinfo.R
@@ -1,14 +1,19 @@
 #SET REQUIRED PASSWORDS
 
-#SET VENUE NAME AND VENUE DISPLAY NAME
-venue <<- "Bananaman`s_Bar_PE27_6TN"
-venueDisplayTitle <<- "Bananaman`s Bar"
+#SET VENUE NAME AND VENUE DISPLAY NAME: 
+#VENUE SHOULD BE THE VENUE NAME AND POSTCODE WITH ANY SPACES REPLACED BY _ AND ANY APOSTROPHES REPLACED BY 1
+#VENUE DISPLAY TITLE SHOULD EXACTLY HOW THE CUSTOMER SHOULD SEE THE NAME
+
+venue <<- "Bananaman1s_Bar_PE27_6TN"
+venueDisplayTitle <<- "Bananaman's Bar"
 
 #SQL database host, port, username and password
+
 Sys.setenv(SQL_ENDPOINT = 'database-2.cnmaqhhd7kkj.eu-west-2.rds.amazonaws.com')
 Sys.setenv(SQL_PORT = 3306)
 Sys.setenv(MY_UID='sqlUsername')
 Sys.setenv(MY_PWD='ReplaceThisPassword')
 
-#SET VENUE LOGIN PASSWORD FOR VENUE END APP NB: NO APOSTROPHES IN THESE 
-Sys.setenv(Venue_PsWd='BetterThanSuperTed001!')
+#VENUE LOGIN PASSWORD FOR PUBEND AND CHECKCLOSED APPS
+
+Sys.setenv(VenuePsWd='BetterThanSuperTed001!')
